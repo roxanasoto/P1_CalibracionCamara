6920ec85bf4f217164d2782b8da1c0af3f5c5b0f
Roxana <roxana.soto.barrera@gmail.com>
2019-01-10 20:14:32 -0500

    Debug_Trabajo1

--- Trabajo 1/codigo/build-RCirculos-Desktop-Debug/moc_mainwindow.cpp
+++ Trabajo 1/codigo/build-RCirculos-Desktop-Debug/moc_mainwindow.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'mainwindow.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.9.5)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.0)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -12,7 +12,7 @@
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'mainwindow.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.9.5. It"
+#error "This file was generated using the moc from 5.12.0. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[9];
-    char stringdata0[127];
+    QByteArrayData data[18];
+    char stringdata0[330];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -39,23 +39,37 @@ QT_MOC_LITERAL(4, 59, 19), // "on_rbCircle_clicked"
 QT_MOC_LITERAL(5, 79, 17), // "on_rbRing_clicked"
 QT_MOC_LITERAL(6, 97, 10), // "closeEvent"
 QT_MOC_LITERAL(7, 108, 12), // "QCloseEvent*"
-QT_MOC_LITERAL(8, 121, 5) // "event"
+QT_MOC_LITERAL(8, 121, 5), // "event"
+QT_MOC_LITERAL(9, 127, 23), // "on_withDistance_toggled"
+QT_MOC_LITERAL(10, 151, 7), // "checked"
+QT_MOC_LITERAL(11, 159, 26), // "on_btnLoadFileDist_clicked"
+QT_MOC_LITERAL(12, 186, 22), // "on_rbCalibNone_clicked"
+QT_MOC_LITERAL(13, 209, 24), // "on_rbCalibOpencv_clicked"
+QT_MOC_LITERAL(14, 234, 23), // "on_rbCalibAnkur_clicked"
+QT_MOC_LITERAL(15, 258, 22), // "on_rbFrmManual_clicked"
+QT_MOC_LITERAL(16, 281, 25), // "on_rbFrmIntervals_clicked"
+QT_MOC_LITERAL(17, 307, 22) // "on_rbFrmRansac_clicked"
 
     },
     "MainWindow\0on_btnLoadVideo_clicked\0\0"
     "on_btnInitProc_clicked\0on_rbCircle_clicked\0"
     "on_rbRing_clicked\0closeEvent\0QCloseEvent*\0"
-    "event"
+    "event\0on_withDistance_toggled\0checked\0"
+    "on_btnLoadFileDist_clicked\0"
+    "on_rbCalibNone_clicked\0on_rbCalibOpencv_clicked\0"
+    "on_rbCalibAnkur_clicked\0on_rbFrmManual_clicked\0"
+    "on_rbFrmIntervals_clicked\0"
+    "on_rbFrmRansac_clicked"
 };
 #undef QT_MOC_LITERAL
 
 static const uint qt_meta_data_MainWindow[] = {
 
  // content:
-       7,       // revision
+       8,       // revision
        0,       // classname
        0,    0, // classinfo
-       5,   14, // methods
+      13,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -63,11 +77,19 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   39,    2, 0x08 /* Private */,
-       3,    0,   40,    2, 0x08 /* Private */,
-       4,    0,   41,    2, 0x08 /* Private */,
-       5,    0,   42,    2, 0x08 /* Private */,
-       6,    1,   43,    2, 0x08 /* Private */,
+       1,    0,   79,    2, 0x08 /* Private */,
+       3,    0,   80,    2, 0x08 /* Private */,
+       4,    0,   81,    2, 0x08 /* Private */,
+       5,    0,   82,    2, 0x08 /* Private */,
+       6,    1,   83,    2, 0x08 /* Private */,
+       9,    1,   86,    2, 0x08 /* Private */,
+      11,    0,   89,    2, 0x08 /* Private */,
+      12,    0,   90,    2, 0x08 /* Private */,
+      13,    0,   91,    2, 0x08 /* Private */,
+      14,    0,   92,    2, 0x08 /* Private */,
+      15,    0,   93,    2, 0x08 /* Private */,
+      16,    0,   94,    2, 0x08 /* Private */,
+      17,    0,   95,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
@@ -75,6 +97,14 @@ static const uint qt_meta_data_MainWindow[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void, 0x80000000 | 7,    8,
+    QMetaType::Void, QMetaType::Bool,   10,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
 
        0        // eod
 };
@@ -90,15 +120,27 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 2: _t->on_rbCircle_clicked(); break;
         case 3: _t->on_rbRing_clicked(); break;
         case 4: _t->closeEvent((*reinterpret_cast< QCloseEvent*(*)>(_a[1]))); break;
+        case 5: _t->on_withDistance_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 6: _t->on_btnLoadFileDist_clicked(); break;
+        case 7: _t->on_rbCalibNone_clicked(); break;
+        case 8: _t->on_rbCalibOpencv_clicked(); break;
+        //case 9: _t->on_rbCalibAnkur_clicked(); break;
+        case 10: _t->on_rbFrmManual_clicked(); break;
+        //case 11: _t->on_rbFrmIntervals_clicked(); break;
+        //case 12: _t->on_rbFrmRansac_clicked(); break;
         default: ;
         }
     }
 }
 
-const QMetaObject MainWindow::staticMetaObject = {
-    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
-      qt_meta_data_MainWindow,  qt_static_metacall, nullptr, nullptr}
-};
+QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
+    &QMainWindow::staticMetaObject,
+    qt_meta_stringdata_MainWindow.data,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
 
 
 const QMetaObject *MainWindow::metaObject() const
@@ -120,13 +162,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 5)
+        if (_id < 13)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 5;
+        _id -= 13;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 5)
+        if (_id < 13)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 5;
+        _id -= 13;
     }
     return _id;
 }
